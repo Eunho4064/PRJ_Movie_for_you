69e0f35110eaf10064ec27a0dae61553fb99c66f
Eunho4064 <unho2614@naver.com>
2021-12-01 15:41:18 +0900

    작업 지시

--- job01_crawling.py
+++ job01_crawling.py
@@ -1,1 +1,9 @@
-print('git test')
+# crawling 작업
+
+# crawling 은 각자 진행하고 빨리 완성되는 코드로 연도를 나눠서 진행하겠습니다.
+# 일단 2020년 개봉작만 crawling 해주시고 저장 형식은 csv 파일로 하겠습니다.
+# 나머지는 연도별로 나눠서 크롤링 해서 합치겠습니다.
+# column 명은 {'title', 'reviews'}로 통일하겠습니다.
+# 파일명은 reviews_0000.csv 형태로 하겠습니다. 0000은 해당 연도입니다.
+# crawling 한 데이터 파일은 아래 링크 공유폴더에 올려주세요
+# https://drive.google.com/drive/folders/1JprpPHRpmA4rdd115VRnbp_42c5Vsofw
